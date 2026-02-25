7fc3e7066eed9a41a0ea5e7b45b4cc29e8b56e61
khushal mahajan <khushalmahjana303@gmail.com>
2026-02-25 15:36:02 +0530

    first commit

--- function.js
+++ function.js
@@ -1,25 +1,14 @@
-
-function abcd(arr) {
-  if (arr.length == 1) {
-    console.log(arr[0]);
-    return;
+let copy = 0
+let con = true
+let ans = 1
+function abcd(n){
+  if(n == 1){
+    console.log(1)
+    return
   }
-  start = 0;
-  j = 0;
-  end = arr.length - 1;
-  // console.log(end)
-  for (start = 0; start <= end; start++) {
-    for (i = 0; i <= j; i++) {
-      process.stdout.write(arr[i] + " ");
-    }
-    j++;
-    console.log();
+  if(n == 2){
+    console.log(1, 1)
+    return
   }
-  arr.shift()
-  abcd(arr)
 }
-abcd([1,2,3,4,5]);
-// arr = [1,2,45,67,87]
-// arr.pop()
-// console.log(arr)
-
+abcd(5)
